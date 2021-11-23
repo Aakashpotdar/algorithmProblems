a7c6ae1fe2808b43f7318b3a0f14c6b8d8edcbc9
aakashpotdar <potdaraakash@gmail.com>
2021-11-23 17:50:37 +0530

    done

---
+++ primePalindrom/primePalindrom/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace primePalindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            checkPrimePalindrom obj = new checkPrimePalindrom();
+
+            obj.opration();
+        }
+    }
+}
---
+++ primePalindrom/primePalindrom/checkPrimePalindrom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace primePalindrom
+{
+    class checkPrimePalindrom
+    {
+        public void check(int number)
+        {
+            int num = number;
+            int rev = 0;
+            while (number > 0)
+            {
+                int temp = number % 10;
+                rev = (rev * 10) + temp;
+                number = number / 10;
+            }
+            if (num == rev)
+            {
+                Console.WriteLine(num + " is prime and pelindrom ");
+            }
+        }
+        public void opration()
+        {
+            Console.WriteLine("hi");
+            int num = 1000;
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                count = 0;
+                for (int j = 1; j < num ; j++)
+                {
+                    if ((i % j) == 0)
+                    {
+                        count++;
+
+                    }
+                }
+
+                if (count == 2)
+                {
+                    //Console.Write(" " + i);
+                    check(i);
+                }
+            }
+        }
+    }
+}
