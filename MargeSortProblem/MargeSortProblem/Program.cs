af4b5a40fbdc8cd8062a85fe1432683a103d1abf
aakashpotdar <potdaraakash@gmail.com>
2021-11-23 16:46:00 +0530

    marge done

---
+++ MargeSortProblem/MargeSortProblem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MargeSortProblem
+{
+    class Program
+    {
+        static void printArray(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; ++i)
+                Console.Write(arr[i] + " ");
+            Console.WriteLine();
+        }
+        public static void Main(String[] args)
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("Given Array");
+            printArray(arr);
+            MergeSort obj = new MergeSort();
+            obj.sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+            printArray(arr);
+        }
+    }
+
+}
+
