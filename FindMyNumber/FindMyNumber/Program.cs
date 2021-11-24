53e9467aa94a67722c4554c19b139462874481a8
aakashpotdar <potdaraakash@gmail.com>
2021-11-24 12:36:23 +0530

    done for my number

---
+++ FindMyNumber/FindMyNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FindMyNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Search obj = new Search();
+            Console.WriteLine("enter the number for limit");
+            int n = int.Parse(Console.ReadLine());
+            obj.searchNumber(n);
+        }
+    }
+}
---
+++ FindMyNumber/FindMyNumber/Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMyNumber
+{
+    public class Search
+    {
+        public void searchNumber(int limit)
+        {
+            int first = 1;
+            int last = limit - 1;
+            int mid = (first + last) / 2;
+            bool a =true;
+            while (a)
+            {
+                Console.WriteLine("the number is " + mid + " then press 'k', press 'l' for number is less than, press'g'for grater than number");
+                char key = char.Parse(Console.ReadLine());
+                switch (key)
+                {
+                    case 'k':
+                        a = false;
+                        break;
+                    case 'l':
+                        last = mid - 1;
+                        mid = (first + last) / 2;
+                        break;
+                    case 'g':
+                        first = mid + 1;
+                        mid = (first + last) / 2;
+                        break;
+                }
+            }
+        }
+    }
+}
