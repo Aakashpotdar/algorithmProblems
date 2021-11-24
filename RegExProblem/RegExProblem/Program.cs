383f45bad75379a4aa7576b833ffcc636667cb01
aakashpotdar <potdaraakash@gmail.com>
2021-11-24 14:34:07 +0530

    regex done

---
+++ RegExProblem/RegExProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RegExProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            changeData obj = new changeData();
+            string lines = "Hello aakash,we have your full name as Aakash potdar in our system.Please,your contact number is 91-7387302460.Please let us know in any type of clerification thankyou BridgeLabz 01/01/2016";
+
+            Console.WriteLine(lines);
+
+            Console.WriteLine("In case you want to edit data type y or press n");
+
+            char key = char.Parse(Console.ReadLine());
+
+            if (key == 'y')
+            {
+                obj.EditData();
+            }
+
+        }
+    }
+}
---
+++ RegExProblem/RegExProblem/changeData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegExProblem
+{
+    class changeData
+    {
+        public void EditData()
+        {
+            Console.WriteLine("enter the name");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter the last name");
+            string lastName = Console.ReadLine();
+           up1: Console.WriteLine("enter the mobile number");
+            string mobileNum = Console.ReadLine();
+            if (checkNumber(mobileNum))
+            {
+                Console.WriteLine("number is updated");
+            }
+            else
+            {
+                Console.WriteLine("invalid number");
+                goto up1;
+            }
+           up2: Console.WriteLine("enter the date");
+            string date = Console.ReadLine();
+            if (checkNumber(mobileNum))
+            {
+                Console.WriteLine("Date is updated");
+            }
+            else
+            {
+                Console.WriteLine("invalid date");
+                goto up2;
+            }
+
+            Console.WriteLine( "Hello {0},we have your full name as {1} {2} in our system.Please,your contact number is 91-{3}.Please let us know in any type of clerification thank you BridgeLabz {4}",name,name,lastName,mobileNum,date);
+
+        }
+        static bool checkNumber(string number)
+        {
+            string Numreg = @"(^[\d]{10}$)";
+            Regex re = new Regex(Numreg);
+
+            if (re.IsMatch(number))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static bool checkDate(string date)
+        {
+            string Datereg = @"(^[\0-3]{1}[\d]{1}/[\0-1]{1}[0-9]{1}/[\d]{4}$)";
+            Regex re = new Regex(Datereg);
+
+            if (re.IsMatch(date))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
